14d390ac4d2f566357485bf11b3a66988529ff1c
Tomohiro Soga <tomohiro_soga@yahoo.co.jp>
2019-12-08 13:08:08 +0900

    manage tfstate remote

--- terraform/terraform.tf
+++ terraform/terraform.tf
@@ -1,3 +1,11 @@
 terraform {
   required_version = "0.12.6"
+
+  backend "remote" {
+    organization = "connpass-map-api"
+
+    workspaces {
+      name = "connpass-map-api-workspace"
+    }
+  }
 }
