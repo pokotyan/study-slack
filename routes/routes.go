d73d359afbda5716ae0c8e55231790300d62bf4f
Tomohiro Soga <tomohiro_soga@yahoo.co.jp>
2020-02-16 22:27:43 +0900

    fix router

--- routes/routes.go
+++ routes/routes.go
@@ -4,12 +4,10 @@ import (
 	"github.com/gin-gonic/gin"
 	middleware "github.com/pokotyan/connpass-map-api/middleware/logging"
 	connpassrouter "github.com/pokotyan/connpass-map-api/routes/connpass"
-	envrouter "github.com/pokotyan/connpass-map-api/routes/env"
 	statusrouter "github.com/pokotyan/connpass-map-api/routes/status"
 )
 
 func Init(router *gin.Engine) {
 	router.GET("/status", statusrouter.Handler, middleware.Logging())
 	connpassrouter.Init(router)
-	envrouter.Init(router)
 }
